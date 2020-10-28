3aa95777f169b9ca2c927697459d95cac876ba44
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-10-27 22:09:38 -0300

    Create models.py

---
+++ update-currency-price/models.py
@@ -0,0 +1,14 @@
+from odoo import fields, models, api
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def update_currency_and_price(self):
+        products = self.env['product.product'].search([('name','=','ADAPTADOR BLUETOOTH 4.0 USB - OEM TP-7183')])
+        #products = self.env['product.product'].search([('currency_id','=',1)])
+        for product in products:
+            product.list_price = product.list_price / 90
+            # el numero por el que divide es el tipo de cambio hardcodeado pero quiero que tome el valor rate de force_currency_id
+            product.force_currency_id = 3
+            #el id 3 es dolares USD
