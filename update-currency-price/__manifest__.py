937b67308f776bf5ea7457d1a16479c44ffe3f8b
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-10-27 22:05:38 -0300

    Create __manifest__.py

---
+++ update-currency-price/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'update currency and price',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Falta Agregar accion automatizada model.update_currency_and_price() ',
+    'depends': ['sale','product','product_currency'],
+    'data': [
+        ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
