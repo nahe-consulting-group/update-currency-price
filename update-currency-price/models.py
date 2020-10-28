0bff19daf46a16a28a7a48386a57a3465afc4b88
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-10-27 23:18:06 -0300

    Update models.py

--- update-currency-price/models.py
+++ update-currency-price/models.py
@@ -7,7 +7,7 @@ class ProductTemplate(models.Model):
     def update_currency_and_price(self):
         #para probar se busca un name específico 
         #products = self.env['product.product'].search([('name','=','ADAPTADOR BLUETOOTH 4.0 USB - OEM TP-7183')])
-        products = self.env['product.template'].search([('currency_id','=',1)])
+        products = self.env['product.product'].search([('currency_id','=',1)])
         for product in products:
             product.list_price = product.list_price / 90
             # el numero por el que divide es el tipo de cambio hardcodeado pero quiero que tome el valor rate de force_currency_id
