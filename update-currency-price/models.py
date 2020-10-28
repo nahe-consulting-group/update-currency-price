7f26af0c8ae394e6e3b6478d6dba1e646a185f11
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-10-28 18:09:20 -0300

    Update models.py

--- update-currency-price/models.py
+++ update-currency-price/models.py
@@ -2,15 +2,15 @@ from odoo import fields, models, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    _inherit = 'res.currency'
     
     @api.model
     def update_currency_and_price(self):
         #para probar se busca un name específico 
         #products = self.env['product.product'].search([('name','=','ADAPTADOR BLUETOOTH 4.0 USB - OEM TP-7183')])
         products = self.env['product.product'].search([('force_currency_id','=',20)])
+        currency_usd = self.env['res.currency'].search([('name','=','USD')
         for product in products:
-            product.list_price = product.list_price * res.currency.currency_id(3).rate
+            product.list_price = product.list_price * currency_usd.rate
             # el numero por el que divide es el tipo de cambio hardcodeado pero quiero que tome el valor rate de force_currency_id
             product.force_currency_id = 3
             #el id 3 es dolares USD en el caso del servidor que estoy probando con odoo 11
